09c74fbb46d7202a58b32a5d2238b6ecbf976df0
Isaac Cohen Tervaert <139313470+icohentervaert@users.noreply.github.com>
2023-09-04 17:29:43 -0600

    add binds, manual run/compile, improve compatability

--- config.lua
+++ config.lua
@@ -5,6 +5,7 @@
 
 local is_windows = vim.fn.has('win32') == 1
 
+local output_file = 'build/$(FNOEXT)'
 if is_windows then
   -- Enable powershell as your default shell
   vim.opt.shell = "pwsh.exe -NoLogo"
@@ -26,6 +27,8 @@ if is_windows then
       ["*"] = "win32yank.exe -o --lf",
     },
   }
+  -- this is mainly for powershell :/
+  output_file = output_file .. '.exe'
 end
 
 lvim.plugins = {
@@ -36,7 +39,76 @@ lvim.plugins = {
   {
     'xeluxee/competitest.nvim',
     dependencies = 'MunifTanjim/nui.nvim',
-    config = function() require('competitest').setup() end,
+    config = function()
+      require('competitest').setup {
+        runner_ui = {
+          interface = "split"
+        },
+        testcases_use_single_file = true,
+        compile_command = {
+          cpp = { exec = 'g++', args = { '-std=c++17', '$(FNAME)', '-o', output_file, '-Wl,--stack,278435456' } },
+        },
+        compile_directory = "./",
+        running_directory = "./build"
+      }
+    end,
   }
 }
 
+-- Key bindings for Competitest commands
+-- browser is set to f2 when on the page, first you do <F2> + r to receive contest
+lvim.keys.normal_mode["<F2>e"] = ":CompetiTest run<CR>"
+lvim.keys.normal_mode["<F2>r"] = ":CompetiTest receive problem<CR>"
+lvim.keys.normal_mode["<F2>rc"] = ":CompetiTest receive contest<CR>"
+
+
+-- Define the function to compile and execute C++ code
+local function RunCppCode()
+  -- Get the base filename without extension
+  local file_name = vim.fn.expand('%:t:r')
+
+  -- Define the path separator based on the operating system
+  local sp = (is_windows and "\\" or "/")
+
+  -- Construct the path to the executable
+  local executable = '"' .. vim.fn.expand('%:p:h') .. sp .. "build" .. sp ..
+      (is_windows and file_name .. ".exe" or file_name) .. '"'
+
+  -- Construct the compile command
+  local compile_cmd = string.format('g++ -std=c++17 "%s" -o %s ', vim.fn.expand('%:p'), executable)
+
+  -- Define linker flags
+  local ld_flags = '-Wl,--stack,278435456'
+
+  -- Quote ld_flags for Windows (PowerShell)
+  if is_windows then
+    ld_flags = '"' .. ld_flags .. '"'
+  end
+
+  -- Append ld_flags to the compile command
+  compile_cmd = compile_cmd .. ' ' .. ld_flags
+
+  -- Define the execute command
+  local execute_cmd = executable
+
+  -- Add .exe extension for Windows and prefix with ".\" for PowerShell
+  if is_windows then
+    execute_cmd = "." .. execute_cmd
+  else
+    execute_cmd = "./" .. execute_cmd
+  end
+
+  -- Construct the full command to execute
+  local full_cmd = compile_cmd .. ' && ' .. execute_cmd
+
+  vim.cmd('vsplit')
+
+  vim.cmd('terminal echo "running: ' .. vim.fn.expand('%:t') .. '" && ' .. full_cmd)
+
+  vim.cmd('wincmd l')
+end
+
+vim.api.nvim_create_user_command('RunCppCode', RunCppCode, { nargs = 0 })
+
+-- Key binding for when Competitest fails/there are specific usecases where I cannot use it.
+vim.api.nvim_set_keymap('n', '<F3>', [[:RunCppCode<CR>]], { noremap = true, silent = true })
